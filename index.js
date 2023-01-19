d585c4fd7ebc141f97e09dea6ed5ffc3e61e4e29
Куаныш Аменов <KuanyshAmenov@Air-Kuanys.Dlink>
2023-01-20 00:36:01 +0600

    added js file

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1
+document.getElementById('age-table');
+// 2
+document.querySelectorAll('#age-list label')
+// 3
+document.querySelector('td')
+// 4
+document.querySelector('form [name=search]')
+// 5
+document.getElementsByTagName('input')[0]
+// 6
+let inputs = document.querySelectorAll ('input')
+inputs[inputs.length-1]
